af2df621f6cbdda0f55abad55d60923b3eeac48b
Steven Nunez <steven.nunez@gmail.com>
2013-01-23 19:36:31 -0500

    failing test for contact

--- week_3_code.rb
+++ week_3_code.rb
@@ -4,4 +4,10 @@ class Contact
 end
 
 describe Contact do
+  it "is initialized with 3 values" do
+    name = "Steven Nunez"
+    email = "steven.nunez@gmail.com"
+    phone = "212-867-5309"
+    contact = Contact.new name, email, phone
+  end
 end
