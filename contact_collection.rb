eb885d90ff24a59ec114a1a3bf3aa950ceab4d87
Steven Nunez <steven.nunez@gmail.com>
2013-01-23 19:59:26 -0500

    Add Find_by_name test

--- contact_collection.rb
+++ contact_collection.rb
@@ -1,9 +1,11 @@
 require 'minitest/autorun'
 require_relative 'contact'
 class ContactCollection
+
   def initialize
     @contacts = []
   end
+
   def add(contact)
     @contacts << contact
   end
@@ -20,5 +22,15 @@ describe ContactCollection do
     collection.count.must_equal 0
     collection.add contact
     collection.count.must_equal 1
+  end
+  it "finds a contact by name" do
+    contact = Contact.new "Guy", "a@b.com", "222-222-2222"
+    contact = Contact.new "Gal", "b@c.com", "555-555-5555"
+    collection = ContactCollection.new
+    collection.add contact
+
+    found = collection.find_by_name "Guy"
+    found.name.must_equal "Guy"
+    found.email.must_equal "a@b.com"
   end
 end
