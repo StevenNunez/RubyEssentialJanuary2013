246feaf10195408a3fc4197db739e730da82853c
Steven Nunez <steven.nunez@gmail.com>
2013-01-23 19:56:02 -0500

    Pass test in collection

--- contact_collection.rb
+++ contact_collection.rb
@@ -1,5 +1,24 @@
 require 'minitest/autorun'
+require_relative 'contact'
+class ContactCollection
+  def initialize
+    @contacts = []
+  end
+  def add(contact)
+    @contacts << contact
+  end
+  
+  def count
+    @contacts.count
+  end
+end
 
 describe ContactCollection do
-
+  it "can have contacts added to it" do
+    contact = Contact.new "Guy", "a@b.com", "222-222-2222"
+    collection = ContactCollection.new
+    collection.count.must_equal 0
+    collection.add contact
+    collection.count.must_equal 1
+  end
 end
